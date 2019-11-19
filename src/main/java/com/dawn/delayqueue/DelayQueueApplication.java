ae9a272067a15f53199414115ff710c5f2d7fdcf
Ruffianjiang <jiangyj0516@outlook.com>
2019-11-20 00:46:12 +0800

    init

--- src/main/java/com/dawn/delayqueue/DelayQueueApplication.java
+++ src/main/java/com/dawn/delayqueue/DelayQueueApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DelayqueueApplication {
+public class DelayQueueApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DelayqueueApplication.class, args);
+        SpringApplication.run(DelayQueueApplication.class, args);
     }
 
 }
