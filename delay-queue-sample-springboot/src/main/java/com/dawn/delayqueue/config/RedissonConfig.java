74884908e148b5c4cc9fdf15ae70a4a9f3da4863
jiang <jiangyj0516@outlook.com>
2022-06-28 10:26:45 +0800

    依赖更新，修复java分析

--- delay-queue-sample-springboot/src/main/java/com/dawn/delayqueue/config/RedissonConfig.java
+++ delay-queue-sample-springboot/src/main/java/com/dawn/delayqueue/config/RedissonConfig.java
@@ -3,6 +3,8 @@ package com.dawn.delayqueue.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,11 +19,18 @@ import java.io.IOException;
  */
 @Configuration
 public class RedissonConfig {
+    private static final Logger logger = LoggerFactory.getLogger(RedissonConfig.class);
 
     @Bean
-    public RedissonClient redisson() throws IOException {
+    public RedissonClient redisson() {
         // 本例子使用的是yaml格式的配置文件，读取使用Config.fromYAML，如果是Json文件，则使用Config.fromJSON
-        Config config = Config.fromYAML(RedissonConfig.class.getClassLoader().getResource("redisson-config.yml"));
+        Config config = null;
+        try {
+            config = Config.fromYAML(RedissonConfig.class.getClassLoader().getResource("redisson-config.yml"));
+        } catch (IOException e) {
+            logger.error("error", e);
+            System.exit(0);
+        }
         return Redisson.create(config);
     }
 
