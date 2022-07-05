14aceffaaeb21b442859c4d7003844df67c53c43
jiang <jiangyj0516@outlook.com>
2022-07-05 14:46:02 +0800

    README.md

--- delay-queue-sample-springboot/src/main/java/com/dawn/delayqueue/DelayQueueApplication.java
+++ delay-queue-sample-springboot/src/main/java/com/dawn/delayqueue/DelayQueueApplication.java
@@ -3,10 +3,11 @@ package com.dawn.delayqueue;
 import com.dawn.delayqueue.config.EnableDelayQueue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 
 
 @EnableDelayQueue
-@SpringBootApplication
+@SpringBootApplication(exclude = {RedisAutoConfiguration.class,})
 public class DelayQueueApplication {
 
     public static void main(String[] args) {
